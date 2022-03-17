a52a2bf5b6bae36205acb206ce2493568687b00c
Zinadin Zidan <zidan.roking@gmail.com>
2022-03-17 13:28:03 +0300

    sweet: Build power stats hardware service mock
    * E BatteryStatsService: Unable to load Power Hal or power.stats HAL
    
    Signed-off-by: ajitlenka30 <ajitlenka30@gmail.com>

--- device.mk
+++ device.mk
@@ -305,6 +305,7 @@ PRODUCT_PACKAGES += \
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power-service.sweet-libperfmgr \
+    android.hardware.power.stats@1.0-service.mock \
     android.hardware.power@1.2.vendor
 
 PRODUCT_SOONG_NAMESPACES += \
