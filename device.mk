c88b52ee8af3570ee185aeb445ab6d642ed37130
marshmello61 <ultramayur123@gmail.com>
2023-08-30 19:16:04 +0530

    sweet: textclassifier: add and ship prebuilt textclassifier
     - Helps fix the delay we have in copying text and that being available in clipboard
    
    Signed-off-by: Mayur <ultramayur123@gmail.com>
    Signed-off-by: kaderbava <ksbava7325@gmail.com>
    Signed-off-by: kssrao13882 <kssrao13882@gmail.com>
    Signed-off-by: ajitlenka30 <ajitlenka30@gmail.com>

--- device.mk
+++ device.mk
@@ -180,6 +180,13 @@ PRODUCT_PACKAGES += \
     libhwbinder \
     libhwbinder.vendor
 
+# Text Classifier
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/textclassifier/actions_suggestions.universal.model:$(TARGET_COPY_OUT_SYSTEM)/etc/textclassifier/actions_suggestions.universal.model \
+    $(LOCAL_PATH)/textclassifier/textclassifier.en.model:$(TARGET_COPY_OUT_SYSTEM)/etc/textclassifier/textclassifier.en.model \
+    $(LOCAL_PATH)/textclassifier/lang_id.model:$(TARGET_COPY_OUT_SYSTEM)/etc/textclassifier/lang_id.model \
+    $(LOCAL_PATH)/textclassifier/textclassifier.universal.model:$(TARGET_COPY_OUT_SYSTEM)/etc/textclassifier/textclassifier.universal.model
+
 # IPACM
 PRODUCT_PACKAGES += \
     ipacm \
