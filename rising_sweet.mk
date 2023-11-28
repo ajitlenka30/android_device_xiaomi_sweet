658bf05b5b3c9ea3ae935a36410933737368fe56
ajitlenka30 <ajitlenka30@gmail.com>
2023-11-28 21:51:56 +0530

    sweet: Welcome RisingOS

--- rising_sweet.mk
+++ rising_sweet.mk
@@ -11,14 +11,24 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from sweet device
 $(call inherit-product, device/xiaomi/sweet/device.mk)
 
-# Inherit some common PixelOS stuff.
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
-TARGET_BOOT_ANIMATION_RES := 1080
+# Inherit some common RisingOS stuff.
+$(call inherit-product, vendor/rising/config/rising.mk)
 
-PRODUCT_NAME := aosp_sweet
+PRODUCT_NAME := rising_sweet
 PRODUCT_DEVICE := sweet
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi Note 10 Pro
 PRODUCT_MANUFACTURER := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+# RisingOS flags
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_ENABLE_PIXEL_FEATURES := true
+RISING_MAINTAINER := Ajit
+RISING_CHIPSET := Snapdragon732G
+RISING_STORAGE := 128GB
+RISING_RAM := 6GB
+RISING_BATTERY := 5020mAh
+RISING_DISPLAY := 1080x2400
+WITH_GMS := true
