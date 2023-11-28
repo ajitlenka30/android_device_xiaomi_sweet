d5a842ff706b7e38e32baf482d793e2f4b10a61f
ajitlenka30 <ajitlenka30@gmail.com>
2023-11-28 23:55:34 +0530

    sweet: added vendorsetup.sh

---
+++ vendorsetup.sh
@@ -0,0 +1,6 @@
+git clone https://github.com/ajitlenka30/device_xiaomi_sm6150-common.git device/xiaomi/sm6150-common
+git clone https://github.com/ajitlenka30/vendor_xiaomi_sm6150-common.git vendor/xiaomi/sm6150-common
+git clone https://github.com/ajitlenka30/android_vendor_xiaomi_sweet.git vendor/xiaomi/sweet
+git clone https://github.com/ajitlenka30/vendor_xiaomi_sweet-miuicamera.git vendor/xiaomi/sweet-miuicamera
+git clone https://github.com/crdroidandroid/android_kernel_xiaomi_sm6150.git kernel/xiaomi/sm6150
+git clone https://github.com/Evolution-X/hardware_xiaomi.git hardware/xiaomi
